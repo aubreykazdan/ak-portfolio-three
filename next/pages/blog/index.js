c0f1d5251236eba61a68fed9c81b875385dfa8c1
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2022-12-29 14:51:42 -0500

    data successfully being retrieved

--- next/pages/blog/index.js
+++ next/pages/blog/index.js
@@ -1,7 +1,13 @@
+import { getClient } from "../../lib/sanity.server";
+import { useRouter } from "next/router";
+
 import SimpleBanner from "../../components/layouts/banners/simpleBanner";
 import Layout from "../../components/layouts/base/layout";
+import { allPostsQuery } from "@/lib/queries";
 
-export default function Blog({}) {
+export default function Blog({ data }) {
+  const router = useRouter();
+  console.log(data);
   return (
     <Layout page="Blog">
       <main>
@@ -9,4 +15,16 @@ export default function Blog({}) {
       </main>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ params, preview = false }) {
+  const blogPosts = await getClient(preview).fetch(allPostsQuery);
+  return {
+    props: {
+      preview,
+      data: {
+        blogPosts,
+      },
+    },
+  };
 }
