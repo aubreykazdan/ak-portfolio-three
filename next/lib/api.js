02ab20150cf54b0f2d5ca2a062ce729f60ee1aff
Aubrey Kazdan <aubrey.j.kazdan@gmail.com>
2023-01-03 18:37:58 -0500

    trying new functions in api.js

--- next/lib/api.js
+++ next/lib/api.js
@@ -11,3 +11,63 @@ const getUniquePosts = (posts) => {
     return true;
   });
 };
+
+const postFields = `
+  _id,
+  body,
+  bodyTwo,
+  categories[]->,
+  description,
+  featured,
+  imagesArray,
+  mainImage,
+  mainImageTwo,
+  'slug': slug.current,
+`;
+
+const getClient = (preview) => (preview ? previewClient : client);
+
+export async function getPreviewPostBySlug(slug) {
+  const data = await getClient(true).fetch(
+    `*[_type == "post" && slug.current == $slug] | order(publishedAt desc){
+      ${postFields}
+    }`,
+    { slug }
+  );
+  return data[0];
+}
+
+export async function getAllPostsWithSlug() {
+  const data = await client.fetch(`*[_type == "post"]{ 'slug': slug.current }`);
+  return data;
+}
+
+export async function getAllPostsForHome(preview) {
+  const results = await getClient(preview)
+    .fetch(`*[_type == "post"] | order(publishedAt desc){
+      ${postFields}
+    }`);
+  return getUniquePosts(results);
+}
+
+export async function getPostAndMorePosts(slug, preview) {
+  const curClient = getClient(preview);
+  const [post, morePosts] = await Promise.all([
+    curClient
+      .fetch(
+        `*[_type == "post" && slug.current == $slug] | order(_updatedAt desc) {
+        ${postFields}
+        }
+      }`,
+        { slug }
+      )
+      .then((res) => res?.[0]),
+    curClient.fetch(
+      `*[_type == "post" && slug.current != $slug] | order(publishedAt desc, _updatedAt desc){
+        ${postFields}
+      }[0...2]`,
+      { slug }
+    ),
+  ]);
+  return { post, morePosts: getUniquePosts(morePosts) };
+}
